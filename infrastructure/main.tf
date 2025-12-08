e70eaceb8986e88518524b556b7377badf854370
leila-charles <pambocharles67@gmail.com>
2025-12-08 10:29:17 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,36 +1,31 @@
-# 1️⃣ Définition du provider Docker
 terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.2"
+      version = "3.0.2"
     }
   }
+
   required_version = ">= 1.0"
 }
 
-# 2️⃣ Connexion à Docker local
 provider "docker" {}
 
-# 3️⃣ Build de l'image Docker à partir du Dockerfile dans demo-devops/
 resource "docker_image" "nginx_image" {
-  name = "leila685/leila_pro:latest"
-
-  build {
-    # Path vers le dossier contenant Dockerfile et fichiers du site
-    context    = "${path.module}/../"
-    dockerfile = "${path.module}/../Dockerfile"
-  }
+  name = "nginx:latest"
 }
 
-# 4️⃣ Création du container NGINX
 resource "docker_container" "nginx_container" {
-  name  = "leila.charles"  # Nom visible dans Docker Desktop
+  name  = "terraform-nginx"
   image = docker_image.nginx_image.name
 
-  # Mappe le port local 8080 vers le port 80 du container
   ports {
     internal = 80
     external = 8080
+  }
+
+  volumes {
+    host_path      = "${demo-devops}/index.html"     # dossier local contenant ton site
+    container_path = "/usr/share/nginx/html"  # emplacement NGINX
   }
 }
