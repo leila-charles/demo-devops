eb396efc483d17f54fc20f0e803e63ff1f879234
leila-charles <pambocharles67@gmail.com>
2025-12-08 12:14:59 +0100

    cc

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
-# On prend une image légère de Nginx
 FROM nginx:alpine
 
-# On supprime la page par défaut de Nginx
 RUN rm -rf /usr/share/nginx/html/*
 
-# On copie notre index.html dans le dossier servi par Nginx
 COPY index.html /usr/share/nginx/html/index.html
 
-# On expose le port 80 du conteneur
 EXPOSE 80
 
-# Commande de démarrage (déjà définie dans l'image nginx, mais on la laisse pour être explicite)
 CMD ["nginx", "-g", "daemon off;"]
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,40 +1,38 @@
+# 1. On définit les outils nécessaires (Le Provider Docker)
 terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.2"
+      version = "~> 3.0.1"
     }
   }
-  required_version = ">= 1.0"
 }
 
+# 2. On connecte Terraform à ton Docker local
 provider "docker" {}
 
-# 1️⃣ Build de l'image Docker depuis le Dockerfile dans demo-devops/
-resource "docker_image" "nginx_image" {
-  name = "leila685/leila_pro:latest"
-
-  build {
-    context    = "${path.module}/../demo-devops"       # dossier contenant Dockerfile + site
-    dockerfile = "${path.module}/../demo-devops/Dockerfile"
-  }
+# 3. L'Image : On demande à Terraform de récupérer l'image Nginx légère
+resource "docker_image" "nginx" {
+  name         = "nginx:alpine"
+  keep_locally = false
 }
 
-# 2️⃣ Création du container NGINX
-resource "docker_container" "nginx_container" {
-  name  = "leila.charles"         # Nom visible dans Docker Desktop
-  image = docker_image.nginx_image.name
+# 4. Le Conteneur : On configure le serveur
+resource "docker_container" "nginx_server" {
+  image = docker_image.nginx.image_id
+  name  = "mon_site_terraform" # Le nom que tu verras dans Docker Desktop
 
-  # Mappe le port local 8080 vers le port 80 du container
+  # On mappe le port 8000 de ton PC vers le port 80 du conteneur
   ports {
     internal = 80
-    external = 8080
+    external = 8000
   }
 
-  # 3️⃣ Surcharge index.html du container avec ton fichier local
+  # C'est la partie magique : on remplace le fichier index.html du conteneur
+  # par TON fichier index.html local.
   volumes {
-    host_path      = abspath("${path.module}/../demo-devops/index.html")
     container_path = "/usr/share/nginx/html/index.html"
+    host_path      = "${path.cwd}/index.html"
     read_only      = true
   }
 }
