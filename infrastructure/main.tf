75273e03d230e6cb5cc65bd8694b82cf822831e7
leila-charles <pambocharles67@gmail.com>
2025-12-08 10:22:10 +0100

    update

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,17 +1,18 @@
+# 1️⃣ On définit le provider Docker
 terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "3.0.2"
+      version = "~> 3.0.2"
     }
   }
-
   required_version = ">= 1.0"
 }
 
+# 2️⃣ On connecte Terraform à Docker local
 provider "docker" {}
 
-# Build l'image Docker à partir du Dockerfile dans demo-devops/
+# 3️⃣ L'image : on build à partir du Dockerfile dans demo-devops/
 resource "docker_image" "nginx_image" {
   name = "leila685/leila_pro:latest"
 
@@ -21,11 +22,12 @@ resource "docker_image" "nginx_image" {
   }
 }
 
-# Lancer le container NGINX localement
+# 4️⃣ Le conteneur : on configure le serveur NGINX
 resource "docker_container" "nginx_container" {
-  name  = "terraform-nginx"
+  name  = "leila.charles"  # Nom visible dans Docker Desktop
   image = docker_image.nginx_image.name
 
+  # Mappe le port 8080 local vers le port 80 du conteneur
   ports {
     internal = 80
     external = 8080
